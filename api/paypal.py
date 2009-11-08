8eff7424c64dfba60bdcad57672745f37d256588
Brian Guertin <dev@brianguertin.com>
2009-11-09 04:00:45 +0800

    Remove Response clas from paypal.py

--- api/paypal.py
+++ api/paypal.py
@@ -213,24 +213,3 @@ def get_express_checkout_details(token):
     """
     return call('GetExpressCheckoutDetails', token=token)
 
-
-class Response(object):
-    def __init__(self, query_string):
-        self.raw = parse_qs(query_string)
-
-    def __str__(self):
-        return str(self.raw)
-
-    def __getattr__(self, key):
-        key = key.upper()
-        try:
-            value = self.raw[key]
-            if len(value) == 1:
-                return value[0]
-            return value
-        except KeyError:
-            raise AttributeError(self)
-
-    def success(self):
-        return self.ack.upper() in ACK_SUCCESS, ACK_SUCCESS_WITH_WARNING
-    success = property(success)
