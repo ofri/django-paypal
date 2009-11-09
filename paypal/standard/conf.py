9c9b5a3ac4654192fd055b093a7ca01d0751135a
frozenskys <richard@frozenskys.co.uk>
2009-11-09 16:59:31 +0000

    get endpoints from settings

--- paypal/standard/conf.py
+++ paypal/standard/conf.py
@@ -3,7 +3,7 @@ from django.conf import settings
 class PayPalSettingsError(Exception):
     """Raised when settings are incorrect."""
     
-# Paypal Test?
+# Paypal Test
 TEST = getattr(settings, "PAYPAL_TEST", True)
 
 if not TEST:
@@ -12,15 +12,22 @@ else:
     RECEIVER_EMAIL = settings.PAYPAL_TEST_RECEIVER_EMAIL
 
 # API Endpoints.
-POSTBACK_ENDPOINT = "https://www.paypal.com/cgi-bin/webscr"
-SANDBOX_POSTBACK_ENDPOINT = "https://www.sandbox.paypal.com/cgi-bin/webscr"
+POSTBACK_ENDPOINT = getattr(settings, "PAYPAL_ENDPOINT", 
+            "https://www.paypal.com/cgi-bin/webscr")
+SANDBOX_POSTBACK_ENDPOINT = getattr(settings, "PAYPAL_TEST_ENDPOINT", 
+            "https://www.sandbox.paypal.com/cgi-bin/webscr"
 
 # Images
-IMAGE = getattr(settings, "PAYPAL_IMAGE", "http://images.paypal.com/images/x-click-but01.gif")
-SUBSCRIPTION_IMAGE = "https://www.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif"
-SANDBOX_IMAGE = getattr(settings, "PAYPAL_SANDBOX_IMAGE", "https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif")
-SUBSCRIPTION_SANDBOX_IMAGE = "https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif"
+IMAGE = getattr(settings, "PAYPAL_IMAGE", 
+            "http://images.paypal.com/images/x-click-but01.gif")
+SUBSCRIPTION_IMAGE = getattr(settings, "PAYPAL_SUBSCRIPTION_IMAGE",
+            "https://www.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif")
+SANDBOX_IMAGE = getattr(settings, "PAYPAL_TEST_IMAGE", 
+            "https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif")
+SUBSCRIPTION_SANDBOX_IMAGE = getattr(settings, "PAYPAL_SUBSCRIPTION_TEST_IMAGE", 
+            "https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif")
 
+            
 if 'paypal.standard.pdt' in settings.INSTALLED_APPS
     try:
         if not TEST:
