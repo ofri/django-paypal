ce28125ba6d77df1b5974a3888e53411ac68b7c1
Michael Thornhill <michael.thornhill@gmail.com>
2011-06-10 01:54:58 +0800

    correct spelling of transaction

--- paypal/standard/models.py
+++ paypal/standard/models.py
@@ -244,7 +244,7 @@ class PayPalStandardBase(Model):
                 if self.payment_status not in self.PAYMENT_STATUS_CHOICES:
                     self.set_flag("Invalid payment_status. (%s)" % self.payment_status)
                 elif self.payment_status != ST_PP_COMPLETED:
-                    self.set_flag("Not a completed transcation. (%s)" % self.payment_status)
+                    self.set_flag("Not a completed transacation. (%s)" % self.payment_status)
                 if duplicate_txn_id(self):
                     self.set_flag("Duplicate txn_id. (%s)" % self.txn_id)
                 if self.receiver_email != RECEIVER_EMAIL:
